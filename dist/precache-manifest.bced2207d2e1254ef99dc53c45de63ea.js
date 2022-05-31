4d773a13e8f68a0f2929cf371c55427ef21c2a05
nangongpo <1844141227@qq.com>
2022-05-31 10:20:10 +0800

    feat: 测试

---
+++ dist/precache-manifest.bced2207d2e1254ef99dc53c45de63ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4c861ed525adeea8223cb1d4e18ce4",
+    "url": "/vant-viewport/index.html"
+  },
+  {
+    "revision": "a802479c76795dd6881ac02b0d357a16",
+    "url": "/vant-viewport/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vant-viewport/robots.txt"
+  },
+  {
+    "revision": "4d67cb73cd080a2ae9aa",
+    "url": "/vant-viewport/static/css/app.aa999581.css"
+  },
+  {
+    "revision": "0135be0c583b4c6f4aed",
+    "url": "/vant-viewport/static/css/chunk-vant.5a60392f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vant-viewport/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3cf62d94728e41b96f94",
+    "url": "/vant-viewport/static/js/about.23c36b93.js"
+  },
+  {
+    "revision": "4d67cb73cd080a2ae9aa",
+    "url": "/vant-viewport/static/js/app.8cb3a8d0.js"
+  },
+  {
+    "revision": "45fa33e32b81257dcfc7",
+    "url": "/vant-viewport/static/js/chunk-core-js.0b0ac271.js"
+  },
+  {
+    "revision": "4c1cf388129a02a7c2a8",
+    "url": "/vant-viewport/static/js/chunk-libs.972a7ae4.js"
+  },
+  {
+    "revision": "0135be0c583b4c6f4aed",
+    "url": "/vant-viewport/static/js/chunk-vant.352b98fa.js"
+  },
+  {
+    "revision": "fe491c3547a89625a0cb",
+    "url": "/vant-viewport/static/js/runtime.8e997933.js"
+  }
+]);
