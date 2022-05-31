99124dd7721acb0c4264d2635e92fef21d738891
nangongpo <1844141227@qq.com>
2022-05-31 10:20:10 +0800

    feat: 测试

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/vant-viewport/precache-manifest.bced2207d2e1254ef99dc53c45de63ea.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (e.data) {
+    if (e.data === 'skipWaiting') {
+      self.skipWaiting()
+    }
+  }
+})
+
+/* eslint-disable no-undef */
+workbox.core.clientsClaim()
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
