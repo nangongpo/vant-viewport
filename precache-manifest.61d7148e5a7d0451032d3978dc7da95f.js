46ac8dc1d9ff262604240eab21e1d3a53f8006ed
nangongpo <1844141227@qq.com>
2022-05-31 14:41:56 +0800

    测试

--- precache-manifest.61d7148e5a7d0451032d3978dc7da95f.js
+++ precache-manifest.61d7148e5a7d0451032d3978dc7da95f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "871c05883470bf9b69233a6e7e9acc04",
+    "revision": "a7936ff1aac37e9ec5b957c8d8787ac6",
     "url": "/vant-viewport/index.html"
   },
   {
@@ -12,12 +12,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vant-viewport/robots.txt"
   },
   {
-    "revision": "2dbfd55aecf71f484061",
-    "url": "/vant-viewport/static/css/app.aa999581.css"
+    "revision": "6e32f0805159c789e1d8",
+    "url": "/vant-viewport/static/css/app.2a05310a.css"
   },
   {
     "revision": "0135be0c583b4c6f4aed",
     "url": "/vant-viewport/static/css/chunk-vant.5a60392f.css"
+  },
+  {
+    "revision": "e4be5ba1cf0a90771dbc",
+    "url": "/vant-viewport/static/css/login.d5db511c.css"
   },
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
@@ -28,12 +32,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vant-viewport/static/js/about.23c36b93.js"
   },
   {
-    "revision": "2dbfd55aecf71f484061",
-    "url": "/vant-viewport/static/js/app.8868cd20.js"
+    "revision": "6e32f0805159c789e1d8",
+    "url": "/vant-viewport/static/js/app.470b65aa.js"
   },
   {
-    "revision": "45fa33e32b81257dcfc7",
-    "url": "/vant-viewport/static/js/chunk-core-js.0b0ac271.js"
+    "revision": "e8bec305e972fb8b9bdf",
+    "url": "/vant-viewport/static/js/chunk-core-js.0a9bf54d.js"
   },
   {
     "revision": "4c1cf388129a02a7c2a8",
@@ -44,7 +48,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vant-viewport/static/js/chunk-vant.352b98fa.js"
   },
   {
-    "revision": "fe491c3547a89625a0cb",
-    "url": "/vant-viewport/static/js/runtime.8e997933.js"
+    "revision": "e4be5ba1cf0a90771dbc",
+    "url": "/vant-viewport/static/js/login.70f32b6e.js"
+  },
+  {
+    "revision": "c9db55ec243c8086ce5b",
+    "url": "/vant-viewport/static/js/runtime.9c61b518.js"
   }
 ]);
